c8a1614db2df43407da26b6e387b1966614725cd
Misaki <42715287+SEUChenyangWei@users.noreply.github.com>
2019-07-23 21:48:21 +0800

    添加相应子文件

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace SpeechRecognitionMod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonRestart = new System.Windows.Forms.Button();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxShow = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(230, 109);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(117, 44);
+            this.buttonPlay.TabIndex = 0;
+            this.buttonPlay.Text = "play & next";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 172);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(494, 151);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // buttonRestart
+            // 
+            this.buttonRestart.Location = new System.Drawing.Point(382, 109);
+            this.buttonRestart.Name = "buttonRestart";
+            this.buttonRestart.Size = new System.Drawing.Size(117, 44);
+            this.buttonRestart.TabIndex = 2;
+            this.buttonRestart.Text = "Restart";
+            this.buttonRestart.UseVisualStyleBackColor = true;
+            this.buttonRestart.Click += new System.EventHandler(this.buttonRestart_Click);
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(89, 109);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(117, 44);
+            this.buttonBack.TabIndex = 3;
+            this.buttonBack.Text = "Back";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(47, 371);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(494, 173);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBoxShow
+            // 
+            this.textBoxShow.Location = new System.Drawing.Point(234, 337);
+            this.textBoxShow.Name = "textBoxShow";
+            this.textBoxShow.ReadOnly = true;
+            this.textBoxShow.Size = new System.Drawing.Size(137, 28);
+            this.textBoxShow.TabIndex = 5;
+            this.textBoxShow.TextChanged += new System.EventHandler(this.textBoxShow_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 556);
+            this.Controls.Add(this.textBoxShow);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.buttonRestart);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonPlay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonRestart;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxShow;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using SpeechLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpeechRecognitionMod
+{
+    public partial class Form1 : Form
+    {
+/*--------------------------------------------------------------------------------*/
+
+       public string[] question = {" 1.我觉得闷闷不乐, 情绪低沉",
+                                "2.  我觉得一天之中早晨最好",
+                                "3.	我一阵阵哭出来或想哭",
+                                "4.  我晚上睡眠不好",
+                                "5.  我吃得跟平常一样多",
+                                "6.  我与异性密切接触时和以往一样感到愉快",
+                                "7.  我发觉我的体重在下降",
+                                "8.  我有便秘的苦恼",
+                                "9.  我心跳比平时快",
+                                "10.  我无缘无故地感到疲乏",
+                                "11.  我的头脑跟平常一样清楚",
+                                "12.  我觉得经常做的事情并没困难",
+                                "13.  我觉得不安而平静不下来",
+                                "14.  我对将来抱有希望",
+                                "15.我比平常容易生气激动",
+                                "16. 我觉得作出决定是容易的",
+                                "17. 我觉得自己是个有用的人，有人需要我",
+                                "18. 我的生活过得很有意思",
+                                "19. 我认为如果我死了别人会生活得更好些",
+                                "20. 平常感兴趣的事我仍然照样感兴趣"};
+        public string answer;    
+/*--------------------------------------------------------------------------------*/
+        public static int num = 20;
+        public int count = 0;
+/*--------------------------------------------------------------------------------*/
+
+        private SRecognition sr;
+        public Form1()
+        {
+            InitializeComponent();
+            string[] fg = { "是的", "我感到", "不安", "困惑" };
+            sr = new SRecognition(fg);
+            textBoxShow.Text = "未进行语音识别";
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            buttonPlay.Enabled = false;
+            if (count >= 1 && count < num)
+            {
+                sr.over();
+                textBoxShow.Text = "语音识别停止";
+                answer += (Convert.ToString(num + 1) + textBox2);
+                textBox2.Text = " ";
+            }       
+            if (count >= num)
+            {
+                 MessageBox.Show("已经问完所有的问题了，可以点击Restart按钮重新开始");
+                
+            }                              
+            else         
+            {
+                textBox1.Text = question[count];
+                SpVoice voice = new SpVoice();
+                voice.Rate = 10; //语速,[-10,10]
+                voice.Volume = 100; //音量,[0,100]
+                voice.Voice = voice.GetVoices().Item(0); //语音库
+                voice.Speak("请开始回答问题" + textBox1.Text);
+                //voice.Speak("请选择：1,没有或很少时间，2,小部分时间，3，相当多时间，4，绝大部分时间");
+                count++;             
+                buttonPlay.Enabled = true;
+                textBoxShow.Text = "语音识别开始";
+                sr.BeginRec(textBox2);
+            }
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+                   
+        }
+
+        private void buttonRestart_Click(object sender, EventArgs e)
+        {
+            count = 0;//重新开始审问
+            sr.over();
+            buttonPlay.Enabled = true;
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            if (count > 0)
+                count--;//返回上一次提问
+            else
+                MessageBox.Show("已经是第一个了！");                   
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxShow_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
